5def7ee338400ab83dccc3dc9bb2a15cde56d85e
Bruno Ribeiro <brunorchaves1@gmail.com>
2022-02-21 11:24:15 -0300

    remove files

---
+++ hands_on_ml/chp_1_percapita.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from pyparsing import delimited_list
+import sklearn.linear_model
+import os
+
+def prepare_country_stats(oecd_bli, gdp_per_capita):
+    oecd_bli = oecd_bli[oecd_bli["INEQUALITY"]=="TOT"]
+    oecd_bli = oecd_bli.pivot(index="Country", columns="Indicator", values="Value")
+    gdp_per_capita.rename(columns={"2015": "GDP per capita"}, inplace=True)
+    gdp_per_capita.set_index("Country", inplace=True)
+    full_country_stats = pd.merge(left=oecd_bli, right=gdp_per_capita,
+                                  left_index=True, right_index=True)
+    full_country_stats.sort_values(by="GDP per capita", inplace=True)
+    remove_indices = [0, 1, 6, 8, 33, 34, 35]
+    keep_indices = list(set(range(36)) - set(remove_indices))
+    return full_country_stats[["GDP per capita", 'Life satisfaction']].iloc[keep_indices]
+
+
+oecd_bli = pd.read_csv("oecd_bli_2015.csv",thousands=',')
+
+gdp_per_capita = pd.read_csv("gdp_per_capita.csv",thousands=',', delimiter = '\t',
+                 encoding= 'latin1',na_values= "n/a")
+
+country_stats = prepare_country_stats(oecd_bli,gdp_per_capita)
+X = np.c_[country_stats["GDP per capita"]]
+y = np.c_[country_stats["Life satisfaction"]]
+
+country_stats.plot(kind= 'scatter',x="GDP per capita", y = 'Life satisfaction')
+plt.show()
+
+model = sklearn.linear_model.LinearRegression()
+
+model.fit(X,y)
+
+X_new = [[22587]]
+print(model.predict(X_new))
+
+
+
+    
